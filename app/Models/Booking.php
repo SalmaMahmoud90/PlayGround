5b4c7253936e82a4b1ef932082016e45e5b8c4dc
smsm95835@gmail.com <smsm95835@gmail.com>
2026-07-09 23:06:15 +0300

    feat(models): add Booking and Review models with eloquent relationships

---
+++ app/Models/Booking.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $fillable = [
+        'start_date_time',
+        'end_date_time',
+        'status',
+        'payment_method',
+        'payment_status',
+        'total_price',
+        'cancelled_at',
+        'play_ground_id', 
+        'user_id', 
+        'coupon_id'
+    ];
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+    public function playGround(){
+        return $this->belongsTo(PlayGround::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function coupon(){
+        return $this->belongsTo(Coupon::class);
+    }
+}
---
+++ app/Models/Review.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    protected $fillable = ['user_id', 'play_ground_id', 'booking_id', 'comment'];
+    
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function playGround(){
+        return $this->belongsTo(PlayGround::class);
+    }
+    public function booking(){
+        return $this->belongsTo(Booking::class);
+    }
+}
