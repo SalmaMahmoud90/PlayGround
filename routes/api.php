c7cb5c9634c0babcab80d4dba575a26231e015a6
asmafakhouri963 <asma.fakhouri963@gmail.com>
2026-07-31 17:44:24 +0300

    Add route for playgrounds in API
    Added a new route for fetching playgrounds.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PlayGroundController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,3 +11,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+// مسار البحث والفلترة للملاعب
+Route::get('/playgrounds', [PlayGroundController::class, 'index']);
